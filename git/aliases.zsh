6dcb18f164156e821c2d5c0ca4c8359b93a07670
Sudarshan Sarolkar <sudarshan.sarolkar@sudarshans-ltm.internal.salesforce.com>
2016-08-03 11:32:17 -0700

    first commit with zsh and vim

---
+++ git/aliases.zsh
@@ -0,0 +1,16 @@
+# git aliases
+alias g='git'
+# alias gl='git pull --prune'
+alias gp='git pull '
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+# alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gdf='git diff-fancy'
+alias gc='git commit'
+#alias gca='git commit -a'
+ alias gco='git checkout'
+# alias gcb='git copy-branch-name'
+# alias gcfg='git ec'
+# alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+# alias purr="git pull --rebase"
